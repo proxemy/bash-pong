4ace073a621a4ffd51088bc5a3de7e98601b5fb0
Proxemy <33573115+proxemy@users.noreply.github.com>
2020-05-19 15:14:31 +0200

    Refactor: Implemented 'field_size' datastructure. Added 'init_vars' method.

--- bash-pong.sh
+++ bash-pong.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
 
 declare tempvar
-declare -i newgame			# bool(0/1)
-declare -a maxfieldsize		# maxfieldsize[0] = Y pos, maxfieldsize[1] = X pos
+declare -A FIELD_SIZE=( ["X"]=0 ["Y"]=0 ["CELL_COUNT"]=0)
 declare -a ball				# ball[0] = Y pos, ball[1] = X pos
 declare -a d_ball			# movement. d_ball[0] = deltaY, d_ball[1] = deltaX
 declare -a field 			# status of every single field
 declare -i ball_field		# ball field in field array
 declare -i player1			# player1 = Y pos, X pos = 2
-declare -i player2			# player2 = Y pos , X pos = (maxfieldsize[1] - 1)
+declare -i player2			# player2 = Y pos , X pos = (FIELD_SIZE["Y"] - 1)
 declare -i activefield		# index for active field
-declare -i playersize		# size of players.
-declare -i framecount		# frame counter
-
-framecount=0
-newgame=1
-maxfieldsize=(`stty size`)
-
-playersize=${maxfieldsize[0]}
-let "playersize/=10"	 	# 10th of Y-maxplayfieldsize
-#echo "playersize = $playersize"
-
-# sets whole field array as empty
-for (( i=0; i<=${maxfieldsize[0]}*${maxfieldsize[1]} - 1; i++ )); do
-	field[$i]=0
-done
-#echo "ERROR at drawing i = \"$i\" with field[\$i]=${field[$i]} and #field[@]=${#field[@]}, exiting"
+declare -i player_height		# size of players.
 
 
-
-function game_loop	# calculates ball and player movement
+function init_vars
 {
-	if (( ${ball[1]} <= 2 )) || (( ${ball[1]} >= ${maxfieldsize[1]} - 2 )); then				# game over cond
+	# set static game variables
+	tty_size=$(stty size)
+	tty_size=( $tty_size )
+
+	FIELD_SIZE["Y"]=${tty_size[0]}
+	FIELD_SIZE["X"]=${tty_size[1]}
+	let FIELD_SIZE["CELL_COUNT"]=${FIELD_SIZE["X"]}*${FIELD_SIZE["Y"]}
+
+	let player_height=${FIELD_SIZE["X"]}/10
+
+	# set the initial random vector of the ball
+	direction_x=$(( 1 - 2 * ($RANDOM % 2) ))
+	direction_y=$(( 1 - 2 * ($RANDOM % 2) ))
+
+	d_ball[0]=$(( ($RANDOM % 4 + 1) * direction_x ))
+	d_ball[1]=$(( ($RANDOM % 2 + 1) * direction_y ))
+
+	# sets whole field array as empty
+	for (( i=0; i<${FIELD_SIZE["CELL_COUNT"]}; i++ )); do
+		field[$i]=0
+	done
+}
+
+
+function game_loop {
+	if (( ${ball[1]} <= 2 )) || (( ${ball[1]} >= ${FIELD_SIZE["Y"]} - 2 )); then				# game over cond
 		calculate_ball_vector "1"
-	elif (( ${ball[0]} <= 2 )) || (( ${ball[0]} >= ${maxfieldsize[0]} - 2 )); then				# ball border bounce
+	elif (( ${ball[0]} <= 2 )) || (( ${ball[0]} >= ${FIELD_SIZE["X"]} - 2 )); then				# ball border bounce
 		calculate_ball_vector "0"
 	elif [ ${field[ (($ball_field-1)) ]} == 3 ] || [ ${field[ (($ball_field+1)) ]} == 3 ]; then	# player collision cond
 		echo "PLAYER COLLISION ball = ${ball[@]}, player1=$player1, player2=$player2"
 		exit
 	fi
 
-	tempvar=$(( (${ball[0]} - 1) * ${maxfieldsize[1]} + ${ball[1]} ))	# sets former ball field to empty
+	tempvar=$(( (${ball[0]} - 1) * ${FIELD_SIZE["Y"]} + ${ball[1]} ))	# sets former ball field to empty
 	field[$tempvar]=0
 
 	ball[0]=$(( ${ball[0]} + ${d_ball[0]} )) # ball movement
@@ -49,35 +57,32 @@ function game_loop	# calculates ball and player movement
 	if (( ${ball[0]} <= 1 )); then			# ball border check, so it cant cross borders or change border fields (no re-init)
 		#let "ball[0] += 1"
 		ball[0]=2
-	elif (( ${ball[0]} >= ${maxfieldsize[0]}-1 )); then
+	elif (( ${ball[0]} >= ${FIELD_SIZE["X"]}-1 )); then
 		#let "ball[0] -= 1"
-		ball[0]=$(( ${maxfieldsize[0]} - 1 ))
+		ball[0]=$(( ${FIELD_SIZE["X"]} - 1 ))
 	fi
 	if (( ${ball[1]} <= 2 )); then
 		#let "ball[1] += 1"
 		ball[1]=3
-	elif (( ${ball[1]} >= ${maxfieldsize[1]}-2 )); then
+	elif (( ${ball[1]} >= ${FIELD_SIZE["Y"]}-2 )); then
 		#let "ball[1] -= 1"
-		ball[1]=$(( ${maxfieldsize[1]} - 3 ))
+		ball[1]=$(( ${FIELD_SIZE["Y"]} - 3 ))
 	fi
 
-	#if (( framecount % 2 == 0 )); then		# player movement
-		if (( $player1 < ball[0] )); then
-			let "player1 += 1"
-		elif (( $player1 > ball[0] )); then
-			let "player1 -= 1"
-		fi
-		if (( $player2 < ball[0] )); then
-			let "player2 += 1"
-		elif (( $player2 > ball[0] )); then
-			let "player2 -= 1"
-		fi
-	#fi
+	if (( $player1 < ball[0] )); then
+		let "player1 += 1"
+	elif (( $player1 > ball[0] )); then
+		let "player1 -= 1"
+	fi
+	if (( $player2 < ball[0] )); then
+		let "player2 += 1"
+	elif (( $player2 > ball[0] )); then
+		let "player2 -= 1"
+	fi
 }
 
 
-function calculate_ball_vector # calculates impact and outbound angle. 0=border bounce, 1=player bounce.
-{
+function calculate_ball_vector {
 	if (( $1 == 0 )); then		# border bounce
 		let "d_ball[0] -= 2 * d_ball[0]"	#inverts delta
 		#echo "new d_ball=\"${d_ball[1]}\""
@@ -90,76 +95,59 @@ function calculate_ball_vector # calculates impact and outbound angle. 0=border 
 }
 
 
-function render_framebuffer # writes the calculated coords to field[]
-{
-
-	tempvar=$(( (${ball[0]} - 1) * ${maxfieldsize[1]} + ${ball[1]} ))	# calculates Xth field[] in array from X/Y ball-coordg
+function render_framebuffer {
+	tempvar=$(( (${ball[0]} - 1) * ${FIELD_SIZE["Y"]} + ${ball[1]} ))	# calculates Xth field[] in array from X/Y ball-coordg
 	field[$tempvar]=3	#sets ball
 	ball_field=$tempvar
 
-	for (( i=2; i<=${maxfieldsize[0]}-1; i++ )); do		# empties the player column so players move and dont expand
-		tempvar=$(( ($i - 1) * ${maxfieldsize[1]} + 2 ))
+	for (( i=2; i<=${FIELD_SIZE["X"]}-1; i++ )); do		# empties the player column so players move and dont expand
+		tempvar=$(( ($i - 1) * ${FIELD_SIZE["Y"]} + 2 ))
 		field[$tempvar]=0	#empties left column
 
-		tempvar=$(( $i * ${maxfieldsize[1]} - 2 ))
+		tempvar=$(( $i * ${FIELD_SIZE["Y"]} - 2 ))
 		field[$tempvar]=0	#empties right column
 	done
 
 	for (( i=playersize; i>=0; i-- )); do					# sets the player bodies in field[] according to player coords
-		tempvar=$(( ($player1 - $i) * ${maxfieldsize[1]} + 2 ))		# calculates player 1 fields
+		tempvar=$(( ($player1 - $i) * ${FIELD_SIZE["Y"]} + 2 ))		# calculates player 1 fields
 		field[$tempvar]=2	#sets player1 UPPER end
-		tempvar=$(( ($player1 + $i) * ${maxfieldsize[1]} + 2 ))		# calculates player 1 fields
+		tempvar=$(( ($player1 + $i) * ${FIELD_SIZE["Y"]} + 2 ))		# calculates player 1 fields
 		field[$tempvar]=2	#sets player1 LOWER end
 
-		tempvar=$(( ($player2 - $i) * ${maxfieldsize[1]} - 2 ))		# calculates player 2 fields
+		tempvar=$(( ($player2 - $i) * ${FIELD_SIZE["Y"]} - 2 ))		# calculates player 2 fields
 		field[$tempvar]=2	#sets player2 UPPER end
-		tempvar=$(( ($player2 + $i) * ${maxfieldsize[1]} - 2 ))		# calculates player 2 fields
+		tempvar=$(( ($player2 + $i) * ${FIELD_SIZE["Y"]} - 2 ))		# calculates player 2 fields
 		field[$tempvar]=2	#sets player2 LOWER end
 	done
 }
 
 
-function init_framebuffer {	# initilizes all fields and coords to start
+function init_framebuffer {
+	for (( i=0; i<${FIELD_SIZE["CELL_COUNT"]}; i++ )); do
 
-	for (( i=0; i<=${maxfieldsize[0]}*${maxfieldsize[1]}-1; i++ )); do
-
-		if (( $i <= ${maxfieldsize[1]} - 1 )) || (( $i >= ${#field[@]} - ${maxfieldsize[1]} - 1 )) || # cond for horizontal borders
-		(( $i % ${maxfieldsize[1]} == 0 )) || (( $i % ${maxfieldsize[1]} == ${maxfieldsize[1]} - 1 )) # cond for vertical borders
+		if (( $i <= ${FIELD_SIZE["X"]} - 1 )) || (( $i >= ${#field[@]} - ${FIELD_SIZE["X"]} - 1 )) || # cond for horizontal borders
+		(( $i % ${FIELD_SIZE["X"]} == 0 )) || (( $i % ${FIELD_SIZE["X"]} == ${FIELD_SIZE["X"]} - 1 )) # cond for vertical borders
 		then
 			field[$i]=1		# sets field border
 		fi
 	done
 
-	if (( $newgame == 1 )); then
-		if (( $RANDOM % 2 == 0 )); then	# sets ball start impulse
-			d_ball[0]=$(( $RANDOM % 4 + 1))
-		else
-			d_ball[0]=-$(( $RANDOM % 4 + 1))
-		fi
-		if (( $RANDOM % 2 == 0 )); then
-			d_ball[1]=$(( $RANDOM % 2 + 1))
-		else
-			d_ball[1]=-$(( $RANDOM % 2 + 1))
-		fi
 
-		ball[0]=${maxfieldsize[0]}	# set ball Y in middle
-		let "ball[0]/=2"
+	ball[0]=${FIELD_SIZE["X"]}	# set ball Y in middle
+	let "ball[0]/=2"
 
-		ball[1]=${maxfieldsize[1]}	# set ball X in middle
-		let "ball[1]/=2"
+	ball[1]=${FIELD_SIZE["Y"]}	# set ball X in middle
+	let "ball[1]/=2"
 
-		player1=${maxfieldsize[0]}	# set player1 in middle
-		let "player1/=2"
+	player1=${FIELD_SIZE["Y"]}	# set player1 in middle
+	let "player1/=2"
 
-		player2=${maxfieldsize[0]}	# set player2 in middle
-		let "player2/=2"
-
-		newgame=0
-	fi
+	player2=${FIELD_SIZE["Y"]}	# set player2 in middle
+	let "player2/=2"
 }
 
 
-function echo_framebuffer {	# summs up all the field[]s in one string and echos it
+function echo_framebuffer {
 	unset tempvar
 	for (( i=0; i<=${#field[@]}-1; i++ )); do		#writes the field status into one string so only one echo is needed
 		if [ ${field[$i]} == 0 ]; then		# draw empty field
@@ -175,24 +163,26 @@ function echo_framebuffer {	# summs up all the field[]s in one string and echos 
 			exit
 		fi
 	done
-	framecount+=1
 	clear
 	echo -n "$tempvar"
 	echo -ne "\rball=\"${ball[@]}\", d_ball=\"${d_ball[@]}\""
 	sleep 0.1
 }
 
-#echo "1 --- ${#field[@]}"
-init_framebuffer
-#echo "2 --- ${#field[@]}"
-#f_set_fields
-#echo "3 --- ${#field[@]} --- ${field[@]}"
-#f_draw_screen
 
-while true; do
-	game_loop
-	render_framebuffer
-	echo_framebuffer
-done
+function main {
+	clear
+	init_vars
+	init_framebuffer
 
-#echo "${#field[@]} --- \"${field[1860]}\""
+	while true; do
+		game_loop
+		render_framebuffer
+		echo_framebuffer
+		#exit
+	done
+}
+
+
+main
+
